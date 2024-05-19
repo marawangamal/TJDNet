0c945b54d8aae3e9a5a0c82f42540f8fa24d4a8d
Marawan Gamal <marawan.gamal@live.com>
2024-05-19 11:44:54 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="foo",
     version="1.0",
-    packages=find_packages(),
+    packages=["TJDNet"],
+    install_requires=[
+        "torch>=2.3.0",
+        "tqdm>=4.66.4",
+        "transformers>=4.40.1",
+        "hydra-core>=1.3.2",
+        "pytorch-lightning>=2.2.4",
+        "tensorboardX>=2.6.2.2",
+        "pydantic>=2.7.1",
+        "datasets>=1.14.0",
+        "tensorboard>=2.6.0",
+    ],
 )
