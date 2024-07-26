a71658d4dcd89823ff1afd4dd92c700d70309adf
Marawan Gamal <marawan.gamal@live.com>
2024-07-26 14:30:12 -0400

    Add get_wikitext2_dataloaders func

--- train_ttdist.py
+++ train_ttdist.py
@@ -1,30 +1,15 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchtext.datasets import PennTreebank
-from torchtext.vocab import vocab
-from collections import Counter, OrderedDict
-from transformers import GPT2Model, GPT2Config
-import torch.optim as optim
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+)
+
 import argparse
-from tqdm import tqdm
 import wandb
 
+from utils.data import get_wikitext2_dataloaders
 from utils.utils import get_experiment_name
-
-
-def build_vocab(data_iter):
-    counter = Counter()
-    for text in data_iter:
-        counter.update(text)
-    # Adding high count for '<unk>' and '<pad>'
-    counter.update({"<unk>": 1e5, "<pad>": 1e5})
-    sorted_vocab_items = sorted(counter.items(), key=lambda x: -x[1])
-    vocab_obj = vocab(OrderedDict(sorted_vocab_items))
-    vocab_obj.set_default_index(
-        vocab_obj["<unk>"]
-    )  # Set default index for unknown tokens
-    return vocab_obj
 
 
 def count_batches(data_loader):
@@ -34,87 +19,50 @@ def count_batches(data_loader):
     return count
 
 
-def decode_sequence(vocab_obj, sequence):
-    decoded_string = "".join([vocab_obj.get_itos()[token_id] for token_id in sequence])
-    return decoded_string
-
-
-def collate_batch(batch, vocab_obj, max_len=512):
-    # Function to handle padding of batch
-    batch_out = [
-        torch.tensor([vocab_obj[char] for char in list(data)], dtype=torch.long)
-        for data in batch
-    ]
-    # Pad sequences in the batch
-    batch_out = torch.nn.utils.rnn.pad_sequence(
-        batch_out, padding_value=vocab_obj["<pad>"], batch_first=True
-    )
-    batch_out = batch_out[:, :max_len]
-    return batch_out
-
-
 class HFTransformerModel(nn.Module):
-    def __init__(self, vocab_size, embed_size, num_layers):
+    def __init__(self):
         super(HFTransformerModel, self).__init__()
-        config = GPT2Config(
-            vocab_size=vocab_size,
-            n_embd=embed_size,
-            n_layer=num_layers,
-            n_head=embed_size // 64,  # Assuming embed size is divisible by 64
+        # self.model = GPT2LMHeadModel.from_pretrained("gpt2")
+        self.model = AutoModelForCausalLM.from_pretrained("gpt2")
+        self.tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.unembedding = nn.Linear(
+            self.model.config.n_embd, self.model.config.vocab_size
         )
-        self.transformer = GPT2Model(config)
-        self.fc_out = nn.Linear(embed_size, vocab_size)
 
-    def forward(self, src):
-        outputs = self.transformer(src, return_dict=True)
-        output = outputs.last_hidden_state
-        return self.fc_out(output)
+    def forward(self, input_ids, attention_mask=None):
+        # Adjust input_ids to create source and target
+        source = input_ids[..., :-1].contiguous()  # All but the last token
+        target = input_ids[..., 1:].contiguous()  # All but the first token
+
+        # Similarly adjust attention_mask to match the source length
+        if attention_mask is not None:
+            adjusted_attention_mask = attention_mask[
+                ..., :-1
+            ].contiguous()  # Match the source shape
+        else:
+            adjusted_attention_mask = None
+
+        # Pass the adjusted attention mask along with source and target
+        outputs = self.model(
+            source, attention_mask=adjusted_attention_mask, labels=target
+        )
+        return outputs.loss
 
 
-def train(
-    model, train_loader, device, optimizer, criterion, num_epochs, total_batches=None
-):
-    total_batches = count_batches(train_loader)
-    decoded_input = ""
-    decoded_output = ""
+def train(model, train_loader, device, optimizer, num_epochs):
+    model.train()
     for epoch in range(num_epochs):
-        model.train()
         total_loss = 0
-        with tqdm(train_loader, total=total_batches) as t:
-            for i, data in enumerate(t):
-                inputs, targets = (
-                    data[:, :-1],
-                    data[:, 1:],
-                )  # Shifted by one for next character prediction
-                inputs, targets = inputs.to(device), targets.to(device)
-                optimizer.zero_grad()
-                output = model(inputs)
-                loss = criterion(
-                    output.reshape(-1, model.fc_out.out_features), targets.reshape(-1)
-                )
-                # Clip gradients to avoid exploding gradients
-                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-                loss.backward()
-                optimizer.step()
-                total_loss += loss.item()
-                # Log to W&B
-                wandb.log(
-                    {
-                        "Loss": loss.item(),
-                    }
-                )
-                if i % 50 == 0:
-                    sample_output = model(inputs[0].unsqueeze(0))
-                    decoded_input = decode_sequence(vocab_obj, inputs[0])
-                    decoded_output = decode_sequence(
-                        vocab_obj, sample_output.argmax(-1)[0]
-                    )
-                t.set_postfix(
-                    loss=loss.item(), sin=decoded_input[:30], sout=decoded_output[:30]
-                )
-        print(
-            f"[Epoch {epoch + 1}], Loss: {total_loss / total_batches}, Input: {decoded_input}, Output: {decoded_output}"
-        )
+        for batch in train_loader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            loss = model(**batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+        print(f"Epoch {epoch+1}: Loss {total_loss / len(train_loader)}")
 
 
 if __name__ == "__main__":
@@ -130,7 +78,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--num_epochs",
         type=int,
-        default=10,
+        default=20,
         help="number of epochs to train (default: 10)",
     )
     parser.add_argument(
@@ -170,28 +118,29 @@ if __name__ == "__main__":
         help="root directory for storing the dataset (default: ./data)",
     )
 
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=512,
+        help="maximum length of the input sequence (default: 512)",
+    )
+
     args = parser.parse_args()
 
-    # Initialize dataset and build vocabulary
-    train_iter = PennTreebank(split="train", root=args.root_dir)
-    vocab_obj = build_vocab(train_iter)
+    model = HFTransformerModel()
+    tokenizer = model.tokenizer
 
-    # Reinitialize the dataset iterator for DataLoader
-    train_iter = PennTreebank(split="train", root=args.root_dir)
-    train_loader = DataLoader(
-        train_iter,
+    train_loader, valid_loader, test_loader = get_wikitext2_dataloaders(
+        cache_dir=args.root_dir,
+        tokenizer=tokenizer,
+        max_length=args.max_length,
         batch_size=args.batch_size,
-        shuffle=True,
-        collate_fn=lambda b: collate_batch(b, vocab_obj),
     )
 
-    # Device and model setup
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = HFTransformerModel(len(vocab_obj), args.embed_size, args.num_layers).to(
-        device
-    )
-    optimizer = optim.AdamW(model.parameters(), lr=args.learning_rate)
-    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+
+    model.to(device)
 
     experiment_config = args.__dict__
     experiment_name = get_experiment_name(experiment_config)
@@ -208,7 +157,6 @@ if __name__ == "__main__":
         train_loader,
         device,
         optimizer,
-        criterion,
         args.num_epochs,
     )
     wandb.finish()
