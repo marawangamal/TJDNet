8c541c5899078b4048c5dbb52402a7b200beb57b
Marawan Gamal <marawan.gamal@live.com>
2024-08-04 22:20:19 -0400

    Add unit tests for umps_select_marginalize_batched

--- __tests__/test_utils.py
+++ __tests__/test_utils.py
@@ -36,7 +36,7 @@ class TestTTDist(unittest.TestCase):
     def test_umps_select_marginalize_batched__shape(self):
         rank = 3
         vocab_size = 4
-        output_size = 3
+        n_core_repititions = 3
         alpha = torch.randn(1, rank)
         beta = torch.randn(1, rank)
         core = torch.randn(1, rank, vocab_size, rank)
@@ -48,7 +48,55 @@ class TestTTDist(unittest.TestCase):
             alpha=alpha,
             beta=beta,
             core=core,
-            n_core_repititions=output_size,
+            n_core_repititions=n_core_repititions,
+            selection_map=selection_map,
+            marginalize_mask=marginalize_mask,
+        )
+        expected_shape = (1, vocab_size)
+        self.assertIsNotNone(result)
+        if result is not None:
+            self.assertEqual(tuple(result.shape), expected_shape)
+
+    def test_umps_select_marginalize_batched__select_only(self):
+        rank = 3
+        vocab_size = 4
+        n_core_repititions = 3
+        alpha = torch.randn(1, rank)
+        beta = torch.randn(1, rank)
+        core = torch.randn(1, rank, vocab_size, rank)
+
+        selection_map = torch.tensor([[1, 1, -1]])
+        marginalize_mask = torch.tensor([[0, 0, 0]])
+
+        result = umps_select_marginalize_batched(
+            alpha=alpha,
+            beta=beta,
+            core=core,
+            n_core_repititions=n_core_repititions,
+            selection_map=selection_map,
+            marginalize_mask=marginalize_mask,
+        )
+        expected_shape = (1, vocab_size)
+        self.assertIsNotNone(result)
+        if result is not None:
+            self.assertEqual(tuple(result.shape), expected_shape)
+
+    def test_umps_select_marginalize_batched__marginalize_only(self):
+        rank = 3
+        vocab_size = 4
+        n_core_repititions = 3
+        alpha = torch.randn(1, rank)
+        beta = torch.randn(1, rank)
+        core = torch.randn(1, rank, vocab_size, rank)
+
+        selection_map = torch.tensor([[-1, -1, -1]])
+        marginalize_mask = torch.tensor([[1, 1, 0]])
+
+        result = umps_select_marginalize_batched(
+            alpha=alpha,
+            beta=beta,
+            core=core,
+            n_core_repititions=n_core_repititions,
             selection_map=selection_map,
             marginalize_mask=marginalize_mask,
         )
@@ -58,7 +106,32 @@ class TestTTDist(unittest.TestCase):
             self.assertEqual(tuple(result.shape), expected_shape)
 
     def test_umps_select_marginalize_batched__values(self):
-        pass
+        rank = 3
+        vocab_size = 4
+        batch_size = 1
+        n_core_repititions = 3
+        expected_output = 44
+        alpha = torch.eye(1, rank)
+        beta = torch.eye(1, rank)
+        core = torch.zeros(1, rank, vocab_size, rank)
+        for i in range(vocab_size):
+            core[0, :, i, :] = torch.eye(rank)
+
+        core[0, 0, 0, 0] = expected_output
+
+        selection_map = torch.tensor([[1, 1, -1]])
+        marginalize_mask = torch.tensor([[0, 0, 0]])
+
+        result = umps_select_marginalize_batched(
+            alpha=alpha,
+            beta=beta,
+            core=core,
+            n_core_repititions=n_core_repititions,
+            selection_map=selection_map,
+            marginalize_mask=marginalize_mask,
+        )
+
+        self.assertEqual(expected_output, result[0, 0].item())
 
 
 if __name__ == "__main__":
