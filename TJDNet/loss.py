Bugfix: normalization for right_res in umps_select_marginalize_batched

TJDNet/loss.py
@@ -1,4 +1,4 @@
-from TJDNet import MPSDist
+from TJDNet import MPSDist, MPSDistBase
 
 
 import torch
@@ -70,7 +70,7 @@ def get_entropy_loss(
 
 
 def get_entropy_loss_stable(
-    ttdist: MPSDist,
+    ttdist: MPSDistBase,
     samples: torch.Tensor,
     eps: float = 1e-6,
     *args,
@@ -91,5 +91,4 @@ def get_entropy_loss_stable(
         - sum([torch.log(z) for z in z_list_select])
         + sum([torch.log(z) for z in z_list_norm])
     ).mean()
-
     return loss

TJDNet/utils.py
@@ -182,8 +182,7 @@ def umps_select_marginalize_batched(
                 for b in range(core.shape[0])
             ]
         )
-        # z_tmp = res_right.sum(dim=1)
-        z_tmp = torch.linalg.norm(res_left, dim=1)
+        z_tmp = torch.linalg.norm(res_right, dim=1)
         norm_consts.append(z_tmp)
         res_right = res_right / z_tmp.unsqueeze(1)
         res_right = torch.einsum("bij, bj->bi", res_right_prime, res_right)