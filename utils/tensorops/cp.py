Bugfix: scale_factor computation in select_from_umps_tensor
- Fixed scale_factor computation in select_from_umps_tensor
- Added scale_factor computation in sum_umps_tensor

utils/tensorops/cp.py
@@ -28,8 +28,8 @@ def select_from_cp_tensor(
     return result.prod(dim=2).sum(dim=1)
 
 
-def sum_cp_tensor(tensor: torch.Tensor) -> torch.Tensor:
-    """Sums the CP tensor.
+def sum_cp_tensor(cp_params: torch.Tensor) -> torch.Tensor:
+    """Sum all elements of a CP tensor representation (batched).
 
     Args:
         tensor (torch.Tensor): CP represention of shape (B, R, T, D)
@@ -37,14 +37,13 @@ def sum_cp_tensor(tensor: torch.Tensor) -> torch.Tensor:
     Returns:
         torch.Tensor: Summed tensor of shape (B)
     """
-    _, _, seq_len, _ = tensor.size()
+    _, _, seq_len, _ = cp_params.size()
     result = None
     for t in range(seq_len):
         if result is None:
-            result = tensor[:, :, t, :].sum(dim=-1)  # (B, R)
+            result = cp_params[:, :, t, :].sum(dim=-1)  # (B, R)
         else:
-            result = (tensor[:, :, t, :] * result.unsqueeze(2)).sum(dim=-1)  # (B, R)
-
+            result = (cp_params[:, :, t, :] * result.unsqueeze(2)).sum(dim=-1)  # (B, R)
     if result is None:
         raise ValueError("Empty tensor")
     return result.sum(dim=1)  # (B, R) -> (B)

utils/tensorops/mps.py
@@ -18,19 +18,46 @@ def select_from_umps_tensor(
     Returns:
         torch.Tensor: Selected elements of shape (B,)
     """
-    batch_size = indices.size(0)
     result = alpha
     scale_factors = []
     for t in range(indices.shape[1]):
         core_select = torch.stack(
             [core[b, :, indices[b, t], :] for b in range(core.shape[0])]
         )  # (B, R, R)
-        scale_factor = torch.linalg.norm(
-            core_select.reshape(batch_size, -1), dim=-1
-        )  # (B,)
-        core_select = core_select / scale_factor.reshape(-1, 1, 1)
+        result_raw = torch.einsum("bi, bij -> bj", result, core_select)
+        scale_factor = torch.linalg.norm(result_raw, dim=-1)  # (B,)
         scale_factors.append(scale_factor)
-        result = torch.einsum("bi, bij -> bj", result, core_select)
+        result = result_raw / scale_factor.unsqueeze(1)
+    result = torch.einsum("bi, bi -> b", result, beta)
+    return result, scale_factors
+
+
+def sum_umps_tensorV2(
+    alpha: torch.Tensor,
+    beta: torch.Tensor,
+    core: torch.Tensor,
+    n_core_repititions: int,
+):
+    """Sum all elements of a uMPS tensor representation (batched).
+
+    Args:
+        alpha (torch.Tensor): Alpha tensor of shape (B, R)
+        beta (torch.Tensor): Beta tensor of shape (B R)
+        core (torch.Tensor): Core tensor of shape (B, R, D, R)
+        indices (torch.Tensor): Indices to select from the tensor of shape (B, H). `H` is horizon
+
+    Returns:
+        torch.Tensor: Selected elements of shape (B,)
+    """
+    batch_size = alpha.size(0)
+    core_margin = core.sum(dim=2)  # (B, R, R)
+    result = alpha
+    scale_factors = []
+    for t in range(n_core_repititions):
+        result_raw = torch.einsum("bi, bij -> bj", result, core_margin)
+        scale_factor = torch.linalg.norm(result_raw, dim=-1)
+        scale_factors.append(scale_factor)
+        result = result_raw / scale_factor.unsqueeze(1)
     result = torch.einsum("bi, bi -> b", result, beta)
     return result, scale_factors
 