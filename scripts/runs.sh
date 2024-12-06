254c77bde6e7781038d3511b8430ed483bc8a0cf
Marawan Gamal <marawan.gamal@live.com>
2024-12-06 07:25:26 -0500

    Update loss comparison curve

--- scripts/runs.sh
+++ scripts/runs.sh
@@ -13,9 +13,21 @@
 
 # Vary Horizon
 # python train.py --model cp --rank 2 --horizon 2 --horizon_eval 2
-python train.py --model cp --rank 2 --horizon 4 --horizon_eval 4
-python train.py --model cp --rank 2 --horizon 8 --horizon_eval 8
+# python train.py --model cp --rank 2 --horizon 4 --horizon_eval 4
+# python train.py --model cp --rank 2 --horizon 8 --horizon_eval 8
 
 # python train.py --model mps --rank 2 --horizon 2 --horizon_eval 2
-python train.py --model mps --rank 2 --horizon 4 --horizon_eval 4
-python train.py --model mps --rank 2 --horizon 8 --horizon_eval 8
+# python train.py --model mps --rank 2 --horizon 4 --horizon_eval 4
+# python train.py --model mps --rank 2 --horizon 8 --horizon_eval 8
+
+
+python train.py --model cp --rank 2 --horizon 8 --horizon_eval 2
+python train.py --model cp --rank 4 --horizon 8 --horizon_eval 4
+python train.py --model cp --rank 8 --horizon 8 --horizon_eval 8
+python train.py --model cp --rank 16 --horizon 8 --horizon_eval 8
+
+
+python train.py --model mps --rank 2 --horizon 8 --horizon_eval 2
+python train.py --model mps --rank 4 --horizon 8 --horizon_eval 4
+python train.py --model mps --rank 8 --horizon 8 --horizon_eval 8
+python train.py --model mps --rank 16 --horizon 8 --horizon_eval 8
