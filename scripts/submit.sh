8cf07dd3b9e077f1bc406fbe90251c0a6b80d01b
marawangamal <44987843+marawangamal@users.noreply.github.com>
2025-01-20 07:30:09 -0500

    Feat/GitHub code (#29)
    * Update cache location
    
    * Add github code dataset

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -9,41 +9,15 @@
 sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head base --horizon 1 --horizon_eval 1 --freeze_base_model --init_method random
 sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head base --horizon 1 --horizon_eval 1 --freeze_base_model --init_method pretrained
 
-# CP (R=2-8)  Does increasing rank help (with hidden_dim=1024)? 
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 1024
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 4 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 1024
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 8 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 1024
-
-# CP (hidden_dim=256-1024) Does increasing hidden_dim help? Yes+
+# CP (hidden_dim=256-1024) Does increasing num_layers help?
 sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 512 --num_layers 2 --use_layer_norm
 sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 512 --num_layers 4 --use_layer_norm
 sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 512 --num_layers 8 --use_layer_norm
 
 
-# CP (use_nonlinearity) Does nonlinearity help? No
-# sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --use_nonlinearity
+sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --horizon 2 --rank 16 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss --hidden_dim 512 --num_layers 4 --use_layer_norm
 
 
-# MPS
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head mps --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-
-
-# uMPS
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head umps --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head umps --horizon 2 --rank 4 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-
-
-# Shakespeare
-# -----------
-
-# Base
-sbatch scripts/gpt.slurm torchrun --nproc_per_node=2 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head base --horizon 1 --horizon_eval 1 --freeze_base_model --use_memory_efficient_loss --init_method random
-sbatch scripts/gpt.slurm torchrun --nproc_per_node=2 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head base --horizon 1 --horizon_eval 1 --freeze_base_model --use_memory_efficient_loss --init_method pretrained
-
-# CP
-sbatch scripts/gpt.slurm torchrun --nproc_per_node=2 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head cp --horizon 2 --rank 2 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/gpt.slurm torchrun --nproc_per_node=2 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head cp --horizon 2 --rank 4 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head cp --horizon 2 --rank 8 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head cp --horizon 2 --rank 16 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
-sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --eval_strategy epoch --eval_steps 1 --epochs 20 --batch_size 32 --seq_len 256 --dataset shakespeare --model_type gpt2 --model_head cp --horizon 2 --rank 32 --horizon_eval 2 --freeze_base_model --use_memory_efficient_loss
+# Can we get decent performance with CP horizon=1?
+sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head base --rank 1 --horizon 1 --horizon_eval 1 --freeze_base_model --init_method random
+sbatch scripts/llama.slurm torchrun --nproc_per_node=4 train.py --epochs 1 --batch_size 32 --seq_len 256 --dataset sharegpt --model_type llama --model_head cp --rank 1 --horizon 1 --horizon_eval 1 --freeze_base_model --init_method random
