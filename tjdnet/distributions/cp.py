Bugfix: padding and attention mask handling
- Remove addition of padding token
- Use $ as padding token
- Handle attention mask during decoding in generate_v3

tjdnet/distributions/cp.py
@@ -105,15 +105,20 @@ class CPDist(BaseDistribution):
                         for b in range(batch_size)
                     ]
                 )  # (B, 1)
-                next_token = top_k_indices[
-                    torch.arange(batch_size), sampled_indices
-                ].squeeze(
-                    1
-                )  # (B,)
+                # next_token = top_k_indices[
+                #     torch.arange(batch_size), sampled_indices
+                # ].squeeze(
+                #     1
+                # )  # (B,)
+                next_token = torch.gather(
+                    top_k_indices, dim=1, index=sampled_indices
+                )  # (B, 1)
             else:
                 # Greedy decoding
-                next_token = torch.argmax(p_ops_tilde, dim=-1).to(dvc)  # (B,)
-            y_hat = torch.cat([y_hat, next_token.unsqueeze(1)], dim=1)
+                next_token = torch.argmax(p_ops_tilde, dim=-1, keepdim=True).to(
+                    dvc
+                )  # (B, 1)
+            y_hat = torch.cat([y_hat, next_token], dim=1)
         return y_hat  # (B, H)
 
     def evaluate_at_points(

tjdnet/models/_tjd.py
@@ -207,9 +207,11 @@ class TJD(ABC, torch.nn.Module):
             param.requires_grad = False
 
     # TODO: use stop_strings to match hf api
+    # TODO: fix output_seqs_completed logic
     def generate_v3(
         self,
         input_ids: torch.Tensor,
+        attention_mask: torch.Tensor,
         stop_token: int,
         max_new_tokens: int = 8,
         do_sample: bool = True,
@@ -225,8 +227,19 @@ class TJD(ABC, torch.nn.Module):
         hidden_state = None
         with torch.no_grad():
             for time_step in range(0, max_new_tokens, horizon):
+                # Need to append 1s column after each generation step
+                _attention_mask = torch.cat(
+                    (
+                        attention_mask,
+                        torch.ones(
+                            (output_seqs_active.size(0), time_step),
+                            device=attention_mask.device,
+                        ),
+                    ),
+                    dim=1,
+                )
                 hidden_state = self.get_last_hidden_state(
-                    output_seqs_active
+                    input_ids=output_seqs_active, attention_mask=_attention_mask
                 )  # (b, t, d)
                 y_hat = self.model_head.sample(
                     hidden_state,

utils/callbacks/eval_gsm8k.py
@@ -1,96 +1,12 @@
 from typing import Optional
 import torch
-from transformers.trainer_callback import TrainerControl, TrainerState
-from transformers.training_args import TrainingArguments
-import wandb
 from tqdm import tqdm
 
 
-from transformers import TrainerCallback
 from utils.train_helpers import get_test_samples
 
 
-# TODO: make generic, just needs a chat template with safe_parse
-# class EvalGSM8KCallback(TrainerCallback):
-# def __init__(
-#     self,
-#     eos_token,
-#     test_dataset,
-#     chat_template: BaseClassifierChatTemplate,
-#     max_new_tokens=500,
-#     top_k=50,
-#     horizon=1,
-#     num_beams=1,
-#     tokenizer=None,
-# ):
-#     self.max_new_tokens = max_new_tokens
-#     self.top_k = top_k
-#     self.horizon = horizon
-#     self.num_beams = num_beams
-#     self.tokenizer = tokenizer
-#     self.eos_token = eos_token
-#     self.chat_template = chat_template
-#     self.test_dataset = test_dataset
-
-#     wandb.define_metric("eval/accuracy-v2", step_metric="global_step")
-
-# def on_step_end(
-#     self,
-#     args,
-#     state,
-#     control,
-#     model=None,
-#     **kwargs,
-# ):
-#     # Only compute on single GPU
-#     if not args.local_rank == 0:
-#         return
-
-#     steps_per_epoch = state.max_steps // state.num_train_epochs
-#     should_eval = state.global_step % steps_per_epoch == 0
-#     if not should_eval:
-#         return
-
-#     accuracy = compute_accuracy(
-#         model,
-#         self.tokenizer,
-#         self.test_dataset,
-#         self.eos_token,
-#         self.chat_template,
-#         max_new_tokens=self.max_new_tokens,
-#         horizon=self.horizon,
-#         top_k=self.top_k,
-#         num_beams=self.num_beams,
-#     )
-
-#     print("\n=== Evaluation at step", state.global_step, "===")
-#     print(f"Accuracy: {accuracy}")
-
-#     wandb.log(
-#         {
-#             "eval/accuracy": accuracy,
-#         },
-#         step=state.global_step,
-#     )
-
-# def on_evaluate(
-#     self,
-#     args: TrainingArguments,
-#     state: TrainerState,
-#     control: TrainerControl,
-#     **kwargs,
-# ):
-#     print("++++ EVALUATE ++++")
-#     wandb.log(
-#         {
-#             "eval/accuracy-v2": torch.rand(1).item(),
-#         },
-#         step=state.global_step,
-#         commit=True,
-#     )
-
-
-def compute_accuracy(
+def compute_accuracy_v1(
     model,
     tokenizer,
     test_dataset,
@@ -102,6 +18,7 @@ def compute_accuracy(
     num_beams=1,
     max_num_samples: Optional[int] = 50,
     prompt="",
+    batchsize=1,
 ):
     model.eval()
     correct = 0
@@ -143,6 +60,67 @@ def compute_accuracy(
             pbar.set_postfix({"accuracy": f"{correct / total:.4f}"})
             if max_num_samples and total >= max_num_samples:
                 break
+    return correct / total
+
+
+# TODO: rename prompt_ids
+def collate_fn(batch, tokenizer):
+    batch_dict = {"input_ids": [item["prompt_ids"] for item in batch]}
+    padded_batch = tokenizer.pad(batch_dict, return_tensors="pt")
+
+    batch_dict_labels = {"input_ids": [item["input_ids"] for item in batch]}
+    padded_batch_labels = tokenizer.pad(batch_dict_labels, return_tensors="pt")
+
+    return {
+        **padded_batch,
+        "labels": padded_batch_labels["input_ids"],
+        "attention_mask_labels": padded_batch_labels["attention_mask"],
+    }
+
+
+def compute_accuracy(
+    model,
+    tokenizer,
+    test_dataset,
+    chat_template,
+    max_new_tokens=125,
+    horizon=1,
+    top_k=50,
+    do_sample=True,
+    batch_size=32,
+    **kwargs,
+):
+
+    dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=lambda x: collate_fn(x, tokenizer),
+    )
+    model.eval()
+    correct = 0
+    total = 0
+    for batch in dataloader:
+        batch = {k: v.to(model.device) for k, v in batch.items()}
+        outputs = model.generate(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            max_new_tokens=max_new_tokens,
+            top_k=top_k,
+            do_sample=do_sample,
+            horizon=horizon,
+            stop_token=tokenizer.eos_token_id,  # For tjd models this causes stopping when end token is reached
+        )  # (batch_size, max_seq_len') max_seq_len' might be less than max_seq_len if all sequences stopped early
+
+        # Batched decoding
+        y_pred = tokenizer.batch_decode(outputs)
+        y_true = tokenizer.batch_decode(batch["labels"])
+
+        # Compute accuracy
+        correct += sum(
+            [chat_template.check_answer(y_pred, y_true, tokenizer.eos_token)]
+        )
+        total += len(batch["input_ids"])
     return correct / total
 
 