4a0e3108723b595a1a646e722f0fc8c7b59e9a93
Marawan Gamal <marawan.gamal@live.com>
2025-05-01 13:07:20 -0400

    Bugfix: cp mixture weights constraint

--- tjdnet/distributions/cpb.py
+++ tjdnet/distributions/cpb.py
@@ -3,8 +3,11 @@ from typing import Optional
 
 import torch
 
-from tjdnet.distributions._base import BaseDistFromLinearConfig, BaseDistribution
-from tjdnet.distributions.tpnet import TensorParamNet, TensorParamNetConfig
+from tjdnet.distributions._base import (
+    BaseDistConfig,
+    BaseDistFromLinearConfig,
+    BaseDistribution,
+)
 from tjdnet.utils import sample_topk
 
 
@@ -18,22 +21,18 @@ class CPBDistConfig:
 
 
 class CPBDist(BaseDistribution):
-    def __init__(self, config: CPBDistConfig):
+    def __init__(self, config: BaseDistConfig):
         self.rank = config.rank
         self.horizon = config.horizon
         self.vocab_size = config.vocab_size
-        self.alpha_func = torch.nn.Linear(
-            in_features=config.in_dim,
+        config.param_net.out_dim_encoder = config.rank * config.horizon
+        config.param_net.out_dim_decoder = config.vocab_size
+        super().__init__(config)
+
+        # cp core weights
+        self.alpha_unnorm_func = torch.nn.Linear(
+            in_features=config.param_net.in_dim,
             out_features=config.rank,
-        )
-        self.param_func = TensorParamNet(
-            config=TensorParamNetConfig(
-                in_dim=config.in_dim,
-                hidden_dim=config.hidden_dim,
-                out_dim_encoder=config.rank * config.horizon,
-                out_dim_decoder=config.vocab_size,
-                positivity_func="none",
-            )
         )
 
     def _get_params(self, last_hidden_state: torch.Tensor, **kwargs):
@@ -63,30 +62,38 @@ class CPBDist(BaseDistribution):
         top_k: int,
         **kwargs,
     ):
-        x = hidden_state
-        p_tilde = self.param_func(x)  # (B, HR, V)
+        x = hidden_state[:, -1]  # (B, D)
+        cores = self.param_func(x)  # (B, HR, V)
         y_out = torch.empty(
             x.size(0), 0, device=x.device, dtype=torch.long
         )  # (B, 0 -> H)
         probs_list = []
+        lsm_alpha = torch.log_softmax(self.alpha_unnorm_func(x), dim=-1)  # (B, R)
         for h in range(self.horizon):
-            p_dists = torch.log_softmax(p_tilde, dim=-1).reshape(
+            log_p_dists = torch.log_softmax(cores, dim=-1).reshape(
                 -1, self.horizon, self.rank, self.vocab_size
             )  # (B, H, R, V)
-            py = (
-                p_dists[:, :h]
-                .gather(-1, y_out.reshape(-1, 1, 1).expand(-1, self.horizon, self.rank))
-                .squeeze(-1)
-            )  # (B, H, R)
-            p_dist = p_dists[:, h]  # (B, R, V)
-            log_p = torch.logsumexp(
-                # (B, R, 1) + (B, R) + (B, R, V) -> (B, V)
-                torch.log(self.alpha_func(x)).unsqueeze(-1)
-                + py.sum(dim=1).unsqueeze(-1)
-                + p_dist,
+            log_ph_dist = log_p_dists[:, h]  # (B, R, V)
+
+            history = lsm_alpha
+            if h > 0:
+                log_pys = (
+                    log_p_dists[:, :h]
+                    .gather(
+                        -1, y_out.reshape(-1, h, 1, 1).expand(-1, -1, self.rank, -1)
+                    )
+                    .squeeze(-1)
+                )  # (B, h-1, R)
+                history += log_pys.sum(dim=1)
+
+            log_ph = torch.logsumexp(
+                # (B, R, 1) + (B, R, V) -> (B, V)
+                # self.log_alpha_func(x).unsqueeze(-1)
+                # + py.sum(dim=1).unsqueeze(-1)
+                history.unsqueeze(-1) + log_ph_dist,
                 dim=1,
             )
-            probs = torch.exp(log_p)
+            probs = torch.exp(log_ph)
             probs_list.append(probs)
             sample_fn = lambda x: (
                 sample_topk(x, top_k=top_k)
@@ -94,7 +101,7 @@ class CPBDist(BaseDistribution):
                 else lambda x: sample_topk(x, top_k=1)
             )
             y = sample_fn(probs)  # (B, V) -> (B, 1)
-            y_out = torch.cat([y_out, y.unsqueeze(1)], dim=1)
+            y_out = torch.cat([y_out, y], dim=1)
 
         return y_out, torch.stack(probs_list, dim=1)  # (B, H), (B, H, V)
 
@@ -103,22 +110,21 @@ class CPBDist(BaseDistribution):
 
         Args:
             x (torch.Tensor): Input features. Shape (B, D). (i.e., last hidden state)
-            y (torch.Tensor): Target labels. Shape (B,).
+            y (torch.Tensor): Target labels. Shape (B, H).
 
         Returns:
             torch.Tensor: Computed loss. Shape (B,).
         """
-        alpha = self.alpha_func(x)  # (B, R)
+        lsm_alpha = torch.log_softmax(self.alpha_unnorm_func(x), dim=-1)  # (B, R)
         p_tilde = self.param_func(x)  # (B, HR, V)
-        p_dists = torch.log_softmax(p_tilde, dim=-1)
-        # h, r =
-        # (B, HR, V) -> (B, HR)
-        py = p_dists.gather(
-            2, y.reshape(-1, 1, 1).expand(-1, self.horizon * self.rank, -1)
+        log_p_dists = torch.log_softmax(p_tilde, dim=-1).reshape(
+            -1, self.horizon, self.rank, self.vocab_size
+        )
+        # (B, H, R, V) -> (B, H, R, 1) -> (B, H, R)
+        log_py_prime = log_p_dists.gather(
+            -1, y.reshape(-1, self.horizon, 1, 1).expand(-1, -1, self.rank, -1)
         ).squeeze(-1)
 
-        log_py_prime = py.reshape(-1, self.horizon, self.rank).prod(dim=1)
-        log_py = torch.logsumexp(
-            torch.log(alpha) + log_py_prime, dim=1
-        )  # (B, R) -> (B,)
+        log_py_pprime = log_py_prime.reshape(-1, self.horizon, self.rank).sum(dim=1)
+        log_py = torch.logsumexp(lsm_alpha + log_py_pprime, dim=1)  # (B, R) -> (B,)
         return -log_py
