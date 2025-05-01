Bugfix: cpb normalization

tests/distributions/test_cpb.py
@@ -54,7 +54,7 @@ class TestCPBDist(unittest.TestCase):
         )
         assert y.shape == (batch_size, horizon)
         assert py.shape == (batch_size, horizon, vocab_size)
-        assert torch.all(py.sum(dim=-1) == 1.0)
+        assert torch.allclose(py.sum(dim=-1), torch.ones_like(py.sum(dim=-1)))
 
 
 if __name__ == "__main__":

tjdnet/distributions/cpb.py
@@ -92,7 +92,7 @@ class CPBDist(BaseDistribution):
                 # + py.sum(dim=1).unsqueeze(-1)
                 history.unsqueeze(-1) + log_ph_dist,
                 dim=1,
-            )
+            ) - torch.logsumexp(history.unsqueeze(-1), dim=1)
             probs = torch.exp(log_ph)
             probs_list.append(probs)
             sample_fn = lambda x: (

utils/accuracy.py
@@ -40,7 +40,7 @@ def compute_accuracy(
     # horizon: int = 1,
     avg_meter_kwargs={},
     generate_kwargs={},
-    verbose=False,
+    verbose=True,
     # **kwargs,
 ):
     dataloader = torch.utils.data.DataLoader(