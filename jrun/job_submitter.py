012db8065441104ce8b3a70acbaf4cf67135a1ef
Marawan Gamal <marawan.gamal@live.com>
2025-05-21 17:15:49 -0400

    Add yaml file handler

--- jrun/job_submitter.py
+++ jrun/job_submitter.py
@@ -1,8 +1,13 @@
+import itertools
 import os
+import random
 import re
 import shlex
 import subprocess
 import tempfile
+from typing import Any, Dict
+
+import yaml
 from jrun._base import JobDB
 from jrun.interfaces import JobSpec
 
@@ -51,7 +56,87 @@ class JobSubmitter(JobDB):
             os.unlink(script_path)
 
     def submit(self, file: str, dry: bool = False):
-        pass
+        """Parse the YAML file and submit jobs."""
+        cfg = yaml.safe_load(open(file))
+
+        preamble_map = {
+            name: "\n".join(lines) for name, lines in cfg["preambles"].items()
+        }
+        root = cfg["group"]
+        # recursively walk the tree
+        self.walk(node=cfg["group"], preamble_map=preamble_map, dry=dry)
+
+    def walk(
+        self,
+        node: Dict[str, Any],
+        preamble_map: Dict[str, str],
+        dry: bool = False,
+        depends_on: list[int] = [],
+        group_name: str = "",
+    ):
+        """Recursively walk the job tree and submit jobs."""
+
+        # Base case (single leaf)
+        if hasattr(node, "command"):
+            # Leaf node
+            # generate rand job id int
+            job_id = random.randint(100000, 999999)
+            job = JobSpec(
+                job_id=job_id,
+                group_name=node.get("name", group_name),
+                command=node["command"],
+                preamble=preamble_map.get(node["preamble"], ""),
+                depends_on=[str(_id) for _id in depends_on],
+            )
+            if dry:
+                print(f"DRY-RUN: {job.to_script()}")
+                return [job_id]
+            else:
+                return [self._submit_jobspec(job)]
+
+        # Base case (sweep)
+        elif node["type"] == "sweep":
+            cmd_template = node["command"]
+            sweep = node["sweep"]
+            # Generate all combinations of the sweep parameters
+            keys = list(sweep.keys())
+            values = list(sweep.values())
+            # Generate all combinations of the sweep parameters
+            combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+            # Iterate over the combinations
+            for i, params in enumerate(combinations):
+                job_id = random.randint(100000, 999999)
+                cmd = cmd_template.format(**params)
+                job = JobSpec(
+                    job_id=job_id,
+                    group_name=node.get("name", group_name),
+                    command=cmd,
+                    preamble=preamble_map.get(node["preamble"], ""),
+                    depends_on=[str(_id) for _id in depends_on],
+                )
+                if dry:
+                    print(f"DRY-RUN: {job.to_script()}")
+                    return [job_id]
+                else:
+                    return [self._submit_jobspec(job)]
+
+        # Recursive case:
+        elif node["type"] == "sequential":
+            # Parallel group
+            depends_on = []
+            for entry in node["jobs"]:
+                job_ids = self.walk(
+                    entry, dry=dry, preamble_map=preamble_map, depends_on=depends_on
+                )
+                if job_ids:
+                    depends_on.extend(job_ids)
+
+        elif node["type"] == "parallel":
+            # Parallel group
+            for entry in node["jobs"]:
+                self.walk(
+                    entry, dry=dry, preamble_map=preamble_map, depends_on=depends_on
+                )
 
     def sbatch(self, args: list):
         # Call sbatch with the provided arguments
