f0a647f47801bf22d629172e9303e61e640f37f4
Marawan Gamal <marawan.gamal@live.com>
2025-07-01 20:43:24 -0400

    Update mhead file names

--- tjdnet/distributions/__init__.py
+++ tjdnet/distributions/__init__.py
@@ -3,17 +3,17 @@ from typing import Dict, Type
 from tjdnet.distributions._base import AbstractDist
 from tjdnet.distributions.stp import STPDist
 from tjdnet.distributions.cp import CPDist
-from tjdnet.distributions.cpb import CPCond
-from tjdnet.distributions.cpc import CPCondl
-from tjdnet.distributions.cpe import CPME
+from tjdnet.distributions.cp_cond import CPCond
+from tjdnet.distributions.cp_condl import CPCondl
+from tjdnet.distributions.cpme import CPME
 from tjdnet.distributions.multihead import MultiHeadDist
 
 
 TJD_DISTS: Dict[str, Type[AbstractDist]] = {
     "stp": STPDist,
     "cp": CPDist,
-    "cpb": CPCond,
-    "cpc": CPCondl,
-    "cpe": CPME,
+    "cp_cond": CPCond,
+    "cp_condl": CPCondl,
+    "cpme": CPME,
     "multihead": MultiHeadDist,
 }
